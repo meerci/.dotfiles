625b45b17987ed9df2d0c8a552916f1d1a0df358
xbao <chenbao.cs@qq.com>
2022-08-17 22:46:39 +0800

    core: formatted

--- install.sh
+++ install.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 set -e
 
-repo_path=`dirname $0`
-repo_path=`cd $repo_path; pwd`
+repo_path=$(dirname $0)
+repo_path=$(
+  cd $repo_path
+  pwd
+)
 
 manage() {
   local src=$1
   local link=$2
-  if [ $2 ]
-  then
+  if [ $2 ]; then
     local link=$repo_path/$link
   else
-    local link=$repo_path/`basename $src`
+    local link=$repo_path/$(basename $src)
   fi
 
   echo -n "install $src -> $link: "
 
   # skip if link exists
-  if [ -L $src -a `readlink $src`=$link ]
-    then
-      echo "exists"
-      return 
-  fi 
+  if [ -L $src -a $(readlink $src)=$link ]; then
+    echo "exists"
+    return
+  fi
 
   # backup old config
-  if [ -e $src ]
-  then
+  if [ -e $src ]; then
     cp -rfH $src $link
     rm -r $src
   fi
 
   # create link
-  if [ -e $link ]
-  then
+  if [ -e $link ]; then
     ln -s $link $src
     echo "success"
   else
     echo "can\'t find both source and target"
     return
   fi
-  
+
 }
 
-source $repo_path/links 
-
+source $repo_path/links
--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -e
 
-repo_path=`dirname $0`
-repo_path=`cd $repo_path; pwd`
+repo_path=$(dirname $0)
+repo_path=$(
+  cd $repo_path
+  pwd
+)
 
 function manage() {
-   local src=$1
-   if [ $2 ]
-   then
-     local link=$repo_path/$link
-   else
-     local link=$repo_path/`basename $src`
-   fi
+  local src=$1
+  if [ $2 ]; then
+    local link=$repo_path/$link
+  else
+    local link=$repo_path/$(basename $src)
+  fi
 
   echo -n "uninstall $src -> $link: "
 
   # skip if link not exist
-  if ! [ -L $src -a `readlink $src`=$link ]
-    then
-      echo "not exist"
-      return
+  if ! [ -L $src -a $(readlink $src)=$link ]; then
+    echo "not exist"
+    return
   fi
   # remove link, and copy config back where it is
   [ -e $src ] && rm -r $src
@@ -28,5 +29,4 @@ function manage() {
 
 }
 
-source $repo_path/links 
-
+source $repo_path/links
