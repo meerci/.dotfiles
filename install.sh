01155d9b50eca34f5a604991646a71250716add1
xbao <chenbao.cs@qq.com>
2023-05-26 14:09:05 +0800

    reformat

--- install.sh
+++ install.sh
@@ -3,41 +3,41 @@ set -e
 
 repo_path=$(dirname $0)
 repo_path=$(
-  cd $repo_path
-  pwd
+	cd $repo_path
+	pwd
 )
 
 manage() {
-  local src=$1
-  local link=$2
-  if [ $2 ]; then
-    local link=$repo_path/$link
-  else
-    local link=$repo_path/$(basename $src)
-  fi
+	local src=$1
+	local link=$2
+	if [ $2 ]; then
+		local link=$repo_path/$link
+	else
+		local link=$repo_path/$(basename $src)
+	fi
 
-  echo -n "install $src -> $link: "
+	echo -n "install $src -> $link: "
 
-  # skip if link exists
-  if [ -L $src -a $(readlink $src)=$link ]; then
-    echo "exists"
-    return
-  fi
+	# skip if link exists
+	if [ -L $src -a $(readlink $src)=$link ]; then
+		echo "exists"
+		return
+	fi
 
-  # backup old config
-  if [ -e $src ]; then
-    cp -rfH $src $link
-    rm -r $src
-  fi
+	# backup old config
+	if [ -e $src ]; then
+		cp -rfH $src $link
+		rm -r $src
+	fi
 
-  # create link
-  if [ -e $link ]; then
-    ln -s $link $src
-    echo "success"
-  else
-    echo "can\'t find both source and target"
-    return
-  fi
+	# create link
+	if [ -e $link ]; then
+		ln -s $link $src
+		echo "success"
+	else
+		echo "can\'t find both source and target"
+		return
+	fi
 
 }
 
--- uninstall.sh
+++ uninstall.sh
@@ -3,29 +3,29 @@ set -e
 
 repo_path=$(dirname $0)
 repo_path=$(
-  cd $repo_path
-  pwd
+	cd $repo_path
+	pwd
 )
 
 function manage() {
-  local src=$1
-  if [ $2 ]; then
-    local link=$repo_path/$link
-  else
-    local link=$repo_path/$(basename $src)
-  fi
+	local src=$1
+	if [ $2 ]; then
+		local link=$repo_path/$link
+	else
+		local link=$repo_path/$(basename $src)
+	fi
 
-  echo -n "uninstall $src -> $link: "
+	echo -n "uninstall $src -> $link: "
 
-  # skip if link not exist
-  if ! [ -L $src -a $(readlink $src)=$link ]; then
-    echo "not exist"
-    return
-  fi
-  # remove link, and copy config back where it is
-  [ -e $src ] && rm -r $src
-  [ -e $link ] && cp -r $link $src
-  echo "success"
+	# skip if link not exist
+	if ! [ -L $src -a $(readlink $src)=$link ]; then
+		echo "not exist"
+		return
+	fi
+	# remove link, and copy config back where it is
+	[ -e $src ] && rm -r $src
+	[ -e $link ] && cp -r $link $src
+	echo "success"
 
 }
 
