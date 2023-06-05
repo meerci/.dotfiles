43d87b1b280bd248abdfebee55b083139d8904b4
xbao <chenbao.cs@qq.com>
2023-06-05 18:38:13 +0800

    nvim: python

--- nvim/lua/lang/python.lua
+++ nvim/lua/lang/python.lua
@@ -4,21 +4,23 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "pyright",
-        "black",
         "ruff-lsp",
+        "pylint",
         "ruff",
+        "isort",
       })
     end,
   },
 
-  -- Setup null-ls with `black`
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources, {
-        nls.builtins.formatting.black,
+        nls.builtins.formatting.isort,
         nls.builtins.formatting.ruff,
+        nls.builtins.formatting.black,
+        nls.builtins.diagnostics.pylint,
       })
     end,
   },
