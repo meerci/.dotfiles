5d800f0643ac1575ae179326def43b06a204b9d4
xbao <chenbao.cs@qq.com>
2023-07-14 20:38:28 +0800

    nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,11 +3,6 @@
 -- Add any additional keymaps here
 --
 
-local Util = require("lazyvim.util")
-local lazyterm = function(cwd)
-  Util.float_term(nil, { cwd = cwd, border = "rounded" })
-end
-
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
@@ -27,14 +22,16 @@ map({ "n", "v" }, "<s-l>", "g_", { desc = "Soft line end" })
 map("n", "<a-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "<a-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 
-map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function()
-  lazyterm(Util.get_root())
-end, { desc = "Terminal (cwd)" })
-map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function()
-  lazyterm(Util.get_root())
-end, { desc = "Terminal (cwd)" })
-
 map("i", "<c-h>", "<left>")
 map("i", "<c-l>", "<right>")
+
+local lazyterm = function(cwd)
+  require("lazyvim.util").float_term(nil, { cwd = cwd, border = "rounded" })
+end
+
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function()
+  lazyterm(require("lazyvim.util").get_root())
+end, { desc = "Terminal (cwd)" })
+
+map("n", "<c-_>", lazyterm, { desc = "Terminal (root dir)" })
--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,17 +1,3 @@
 return {
-  {
-    "echasnovski/mini.comment",
-    opts = {
-      comment_line = "<c-_>",
-    },
-    event = "VeryLazy",
-    init = function()
-      vim.keymap.set(
-        "n",
-        "<c-_>",
-        "<cmd>lua require('mini.comment').toggle_lines(vim.fn.line('.'), vim.fn.line('.'))<cr>j",
-        { desc = "Comment Line" }
-      )
-    end,
-  },
+  { "echasnovski/mini.comment" },
 }
