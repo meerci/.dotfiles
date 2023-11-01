088d54a63bc3ac0a2cf1cdca2024173f8c16baf2
xbao <chenbao.cs@qq.com>
2023-11-01 17:25:00 +0800

    nvim: ...

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lang/python" },
     { import = "lang/cpp" },
     { import = "lang/sql" },
+    { import = "lang/json" },
     -- { import = "lang/go" },
   },
   defaults = {
