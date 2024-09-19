eece1fd286698459b6d8dba4b6985fece32ced9d
chenbao <chenbao@baidu.com>
2024-09-19 12:14:51 +0800

    nvim: send to term

--- .config/nvim/lua/plugins/send_to_term.lua
+++ .config/nvim/lua/plugins/send_to_term.lua
@@ -1,9 +1,19 @@
-local function get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
+local M = {}
+M.set_opfunc = vim.fn[vim.api.nvim_exec(
+  [[
+  func s:set_opfunc(val)
+    let &opfunc = a:val
+  endfunc
+  echon get(function('s:set_opfunc'), 'name')
+]],
+  true
+)]
+
+M.get_selection = function(p_start, p_end)
+  local s_start = vim.fn.getpos(p_start)
+  local s_end = vim.fn.getpos(p_end)
   local n_lines = math.abs(s_end[2] - s_start[2]) + 1
   local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
   if n_lines == 1 then
     lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
   else
@@ -12,36 +22,59 @@ local function get_visual_selection()
   return table.concat(lines, "\n")
 end
 
-function Send_To_Terminal()
-  if false == pcall(vim.api.nvim_get_chan_info, vim.g.marked_terminal_chan) then
-    vim.notify("Must select a terminal", vim.log.levels.ERROR)
-    return
+M.MarkTerminal = function(bufnr)
+  if bufnr == nil then
+    bufnr = vim.api.nvim_get_current_buf()
   end
 
-  local input
-  local mode = vim.api.nvim_get_mode().mode
-  if mode == "n" then
-    input = vim.api.nvim_get_current_line()
-  elseif mode == "v" then
-    input = get_visual_selection()
-    -- exit visiual mode
-    vim.fn.feedkeys(":", "nx")
-  end
-  vim.api.nvim_chan_send(vim.g.marked_terminal_chan, input .. "\r")
-end
-
-function MarkTerminal()
   vim.g.marked_terminal_chan = nil
-  local bufnr = vim.api.nvim_get_current_buf()
   for _, v in ipairs(vim.api.nvim_list_chans()) do
     if v.buffer == bufnr then
       vim.g.marked_terminal_chan = v.id
-      return
+      break
     end
   end
-  vim.notify("Must select a terminal", vim.log.levels.ERROR)
+  if vim.g.marked_terminal_chan == nil then
+    vim.notify("This buffer is not a terminal", vim.log.levels.ERROR)
+  else
+    vim.notify("Select Terminal " .. bufnr, vim.log.levels.INFO)
+  end
 end
 
-vim.keymap.set("n", "<leader>R", MarkTerminal, { silent = true, desc = "Mark Terminal" })
-vim.keymap.set({ "n", "v" }, "<leader>r", Send_To_Terminal, { silent = true, desc = "Send text to terminal" })
+M.SendToTerm = function(text)
+  if false == pcall(vim.api.nvim_get_chan_info, vim.g.marked_terminal_chan) then
+    vim.notify("Terminal buffer doest not exist", vim.log.levels.ERROR)
+    return
+  end
+  vim.api.nvim_chan_send(vim.g.marked_terminal_chan, text)
+end
+
+M.SendVisiualToTerm = function()
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == "v" or mode == "V" then
+    -- exit visiual mode
+    vim.api.nvim_feedkeys('\027', 'xt', false)
+    local text = M.get_selection("'<", "'>")
+    M.SendToTerm(text .. "\r")
+  else
+    vim.notify("Unsupported mode: " .. mode)
+  end
+end
+
+M.SendMotionToTerm = function(motion)
+  if motion == nil then
+    M.set_opfunc(M.SendMotionToTerm)
+    vim.api.nvim_feedkeys("g@", "n", false)
+  else
+    M.SendToTerm(M.get_selection("'[", "']") .. "\r")
+  end
+end
+
+vim.keymap.set("n", "<leader>rm", M.MarkTerminal, { silent = true, desc = "Mark Terminal" })
+vim.keymap.set("v", "<leader>r", M.SendVisiualToTerm, { silent = true, desc = "Send visiual to terminal" })
+vim.keymap.set("n", "<leader>rs", M.SendMotionToTerm, { silent = true, desc = "Send motion text to terminal" })
+vim.keymap.set("n", "<leader>rr", function()
+  M.SendToTerm(vim.api.nvim_get_current_line() .. "\r")
+end, { noremap = true, silent = true, desc = "Send line to terminal" })
+
 return {}
